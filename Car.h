301369a56df633e993672cda80cd7c214ad328d8
Hemant Torsekar <hvt@me.com>
2015-04-14 02:04:04 -0400

    Car implementation for
    - (void)startEngine;
    - (void)driveForDistance:(NSNumber *)theDistance;
    - (void)turnByAngle:(NSNumber *)theAngle

--- Car.h
+++ Car.h
@@ -10,42 +10,12 @@
 
 @interface Car : NSObject
 
-// Accessors
-- (BOOL)isRunning;
-- (void)setRunning:(BOOL)running;
-- (NSString *)model;
-- (void)setModel:(NSString *)model;
+@property (copy) NSString *model;
 
-// Calculated values
-- (double)maximumSpeed;
-- (double)maximumSpeedUsingLocale:(NSLocale *)locale;
-
-// Action methods
 - (void)startEngine;
-- (void)driveForDistance:(double)theDistance;
-- (void)driveFromOrigin:(id)theOrigin toDestination:(id)theDestination;
-- (void)turnByAngle:(double)theAngle;
-- (void)turnToAngle:(double)theAngle;
-
-// Error handling methods
-- (BOOL)loadPassenger:(id)aPassenger error:(NSError **)error;
-
-// Constructor methods
-- (id)initWithModel:(NSString *)aModel;
-- (id)initWithModel:(NSString *)aModel mileage:(double)theMileage;
-
-// Comparison methods
-- (BOOL)isEqualToCar:(Car *)anotherCar;
-- (Car *)fasterCar:(Car *)anotherCar;
-- (Car *)slowerCar:(Car *)anotherCar;
-
-// Factory methods
-+ (Car *)car;
-+ (Car *)carWithModel:(NSString *)aModel;
-+ (Car *)carWithModel:(NSString *)aModel mileage:(double)theMileage;
-
-// Singleton methods
-+ (Car *)sharedCar;
+- (void)driveForDistance:(NSNumber *)theDistance;
+- (void)turnByAngle:(NSNumber *)theAngle
+            quickly:(NSNumber *)useParkingBrake;
 
 
 @end
